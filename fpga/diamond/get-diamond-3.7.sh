00953e83de237dcddb45585c40fe4248b19ef5a1
davor <davor@ssd.lan>
2018-04-21 18:57:47 +0200

    lattice diamond installation helper scripts for linux

---
+++ fpga/diamond/get-diamond-3.7.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#wget -c http://files.latticesemi.com/Diamond/3.6/diamond_3_6-base_x64-83-4-x86_64-linux.rpm
+wget -c http://files.latticesemi.com/Diamond/3.7/diamond_3_7-base-96-1-i386-linux.rpm
+"wget -c http://files.latticesemi.com/Diamond/3.7/diamond_3_7-base_x64-96-1-x86_64-linux.rpm
+#wget -c http://files.latticesemi.com/Diamond/3.8/diamond_3_8-base_x64-115-3-x86_64-linux.rpm
