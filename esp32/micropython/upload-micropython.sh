f2af87690b3410ce119a3b08c5340a280e4ed03d
emard <vordah@gmail.com>
2019-11-17 13:06:44 +0100

    fix micropython upload script

--- esp32/micropython/upload-micropython.sh
+++ esp32/micropython/upload-micropython.sh
@@ -1,7 +1,12 @@
-#!/bin/sh -e
+#!/bin/sh
+
+ujprog ../../fpga/passthru/passthru-v20-12f/passthru_ulx3s_v20_12k.bit
+ujprog ../../fpga/passthru/passthru-v20-25f/passthru_ulx3s_v20_25k.bit
+ujprog ../../fpga/passthru/passthru-v20-45f/passthru_ulx3s_v20_45k.bit
+ujprog ../../fpga/passthru/passthru-v20-85f/ulx3s_85f_passthru.bit
 
 python ../serial-uploader/esptool.py --port /dev/ttyUSB0 --baud 460800 erase_flash
 
 python ../serial-uploader/esptool.py --port /dev/ttyUSB0 \
   --baud 460800 write_flash -z 0x1000 \
-  micropython/esp32-idf3-20191117-v1.11-580-g973f68780.bin
+  esp32-idf3-20191117-v1.11-580-g973f68780.bin
