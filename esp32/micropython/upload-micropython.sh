6fbdebd60823d4160e86446c73588e9981c9c10e
emard <vordah@gmail.com>
2021-11-05 14:45:04 +0100

    upload micropython v1.14

--- esp32/micropython/upload-micropython.sh
+++ esp32/micropython/upload-micropython.sh
@@ -9,4 +9,4 @@ python ../serial-uploader/esptool.py --port /dev/ttyUSB0 --baud 460800 erase_fla
 
 python ../serial-uploader/esptool.py --port /dev/ttyUSB0 \
   --baud 460800 write_flash -z 0x1000 \
-  esp32-idf3-20191117-v1.11-580-g973f68780.bin
+  esp32-idf3-20210202-v1.14.bin
